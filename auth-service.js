0a15b7f5ac7313566ce421f6361b198781c81a45
Mahin2312 <mahinibnealam@gmail.com>
2025-02-20 17:18:00 -0500

    test

--- auth-service.js
+++ auth-service.js
@@ -1,4 +1,3 @@
-const bcrypt = require('bcryptjs');
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
@@ -51,29 +50,23 @@ module.exports.registerUser = function (userData) {
         if (userData.password !== userData.password2) {
             reject("Passwords do not match");
         } else {
-            bcrypt.hash(userData.password, 10) // Hash the password
-                .then((hashedPassword) => {
-                    let newUser = new User({
-                        userName: userData.userName,
-                        password: hashedPassword, // Save the hashed password
-                        email: userData.email,
-                        loginHistory: []
-                    });
+            let newUser = new User({
+                userName: userData.userName,
+                password: userData.password, // No hashing
+                email: userData.email,
+                loginHistory: []
+            });
 
-                    newUser.save()
-                        .then(() => {
-                            resolve();
-                        })
-                        .catch((err) => {
-                            if (err.code === 11000) {
-                                reject("User Name already taken");
-                            } else {
-                                reject("There was an error creating the user: " + err);
-                            }
-                        });
+            newUser.save()
+                .then(() => {
+                    resolve();
                 })
                 .catch((err) => {
-                    reject("There was an error encrypting the password");
+                    if (err.code === 11000) {
+                        reject("User Name already taken");
+                    } else {
+                        reject("There was an error creating the user: " + err);
+                    }
                 });
         }
     });
@@ -86,31 +79,25 @@ module.exports.checkUser = function (userData) {
                 if (users.length === 0) {
                     reject(`Unable to find user: ${userData.userName}`);
                 } else {
-                    bcrypt.compare(userData.password, users[0].password) // Compare the entered password with the hashed password
-                        .then((result) => {
-                            if (result === false) {
-                                reject(`Incorrect Password for user: ${userData.userName}`);
-                            } else {
-                                users[0].loginHistory.push({
-                                    dateTime: new Date().toString(),
-                                    userAgent: userData.userAgent
-                                });
-
-                                User.updateOne(
-                                    { userName: users[0].userName },
-                                    { $set: { loginHistory: users[0].loginHistory } }
-                                )
-                                    .then(() => {
-                                        resolve(users[0]);
-                                    })
-                                    .catch((err) => {
-                                        reject("There was an error verifying the user: " + err);
-                                    });
-                            }
-                        })
-                        .catch(() => {
-                            reject("Error comparing passwords");
+                    if (userData.password !== users[0].password) { // No hashing comparison
+                        reject(`Incorrect Password for user: ${userData.userName}`);
+                    } else {
+                        users[0].loginHistory.push({
+                            dateTime: new Date().toString(),
+                            userAgent: userData.userAgent
                         });
+
+                        User.updateOne(
+                            { userName: users[0].userName },
+                            { $set: { loginHistory: users[0].loginHistory } }
+                        )
+                            .then(() => {
+                                resolve(users[0]);
+                            })
+                            .catch((err) => {
+                                reject("There was an error verifying the user: " + err);
+                            });
+                    }
                 }
             })
             .catch(() => {
